2dca36966f74f9b7682303308ce10dae61ef40fb
Steven Luck <steve.luck@gmail.com>
2020-03-24 14:05:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay (musicians,instruments){
+  let output = [];
+  
+  for(let i=0;i<musicians.length;i++) {
+    output.push(musicians[i]+" plays "+instruments[i]);
+  }
+  return(output);
+}
+
+function johnLennonFacts (facts){
+  while 
+}
